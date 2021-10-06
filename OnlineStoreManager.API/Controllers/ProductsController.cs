7ff7f242b78de4f58efec2f5aa899a492d4fa492
Sonu Kumar <devsonuk02@gmail.com>
2021-10-07 01:15:38 +0530

    Revert the changes.

--- OnlineStoreManager.API/Controllers/ProductsController.cs
+++ OnlineStoreManager.API/Controllers/ProductsController.cs
@@ -5,7 +5,7 @@ using System.Web.Http;
 
 namespace OnlineStoreManager.API.Controllers
 {
-    [Authorize(Roles = "Manager")]
+    [Authorize(Roles = "Cashier")]
     public class ProductsController : ApiController
     {
         private readonly GenericRepository<Product> _productRepository;
